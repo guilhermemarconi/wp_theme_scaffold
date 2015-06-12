0f95d22ed0ffd237f45f38e55aec8e15a4dd1e47
Guilherme Marconi <guiilhermemarconi@gmail.com>
2015-06-11 23:38:59 -0300

    Gruntfile.js initial config

---
+++ Gruntfile.js
@@ -0,0 +1,181 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: "**/*.php"
+      },
+      sass: {
+        files: "dev/scss/**/*.scss",
+        tasks: ["sass", "cmq", "postcss"]
+      },
+      js: {
+        files: ["dev/js/**/*.js", "Gruntfile.js"],
+        tasks: ["uglify"]
+      },
+      images: {
+        files: "dev/img/**/*.{png,jpg,gif}",
+        tasks: ["imagemin"]
+      },
+      svg: {
+        files: "dev/svg/**/*.svg",
+        tasks: ["svgmin"]
+      }
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'dev/scss/style.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer-core')({browsers: 'last 1 version'})
+        ]
+      },
+      dist: {
+        src: 'style.css'
+      }
+    },
+
+    cmq: {
+      options: {
+        log: true
+      },
+	    dist:{
+	      files: {
+	        'style.css': 'style.css'
+	      }
+	    }
+    },
+
+    uglify: {
+      options: {
+        mangle: true
+      },
+      production: {
+        files: {
+          "js/scripts.js": "dev/js/scripts.js"
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 7,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dev/img/',
+          src: '**/*.{png,jpg,gif}',
+          dest: 'img/'
+        }]
+      }
+    },
+
+    svgmin: {
+      options: {
+        plugins: [{
+          removeViewBox: false
+        }, {
+          removeUselessStrokeAndFill: false
+        }, {
+          removeEmptyAttrs: false
+        }]
+      },
+      dist: {
+        expand: true,
+        cwd: 'dev/svg/',
+        src: '*.svg',
+        dest: 'includes/svg'
+      }
+    },
+
+    browserSync: {
+      files: {
+        src : [
+        	'style.css',
+        	'**/*.php',
+        ]
+      },
+      options: {
+        watchTask: true,
+        proxy: "dev",
+        ghostMode: {
+        	clicks: true,
+          scroll: true,
+          links: true,
+          forms: true
+        }
+      }
+    },
+
+    ftpush: {
+      build: {
+        auth: {
+          host: "ftp.domain.com",
+          port: 21,
+          authKey: "key1"
+        },
+        src: ".",
+        dest: "/www/wp-content/themes/mytheme/",
+        exclusions: [
+          ".DS_Store",
+          "Thumbs.db",
+          "Gruntfile.js",
+          "package.json",
+          "config.rb",
+          "node_modules",
+          ".ftppass",
+          ".gitignore",
+          ".git",
+          ".sass-cache",
+          "dev"
+        ]
+      }
+    },
+
+    devUpdate: {
+      main: {
+        options: {
+          updateType: 'force',
+          reportUpdated: false,
+          semver: false,
+          packages: {
+            devDependencies: true,
+            dependencies: false
+          },
+          packageJson: null,
+          reportOnlyPkgs: []
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask("default", ["browserSync", "watch"]);
+  grunt.registerTask("css", ["sass", "cmq", "postcss"]);
+  grunt.registerTask("img", ["imagemin"]);
+  grunt.registerTask("js", ["uglify"]);
+  grunt.registerTask("svg", ["svgmin"]);
+  grunt.registerTask("update", ["devUpdate"]);
+  grunt.registerTask("deploy", ["sass", "cmq", "postcss", "imagemin", "uglify", "svgmin", "ftpush"]);
+
+};
